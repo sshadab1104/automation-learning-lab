96d9c2bd9d549b524cdf2dce0b7c17458c4ad653
Shadab Shaikh <sshadab1104@gmail.com>
2026-07-20 01:14:43 +0530

    Completed Day 1 exercise

--- src/J01_Variables_And_DataTypes/Day_01/Ex01_PrintName.java
+++ src/J01_Variables_And_DataTypes/Day_01/Ex01_PrintName.java
@@ -1,4 +1,8 @@
 package J01_Variables_And_DataTypes.Day_01;
 
 public class Ex01_PrintName {
+    public static void main(String[] args) {
+        String name = "Shadab";
+        System.out.println(name);
+    }
 }
